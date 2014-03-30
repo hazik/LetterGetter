bfab1faab0cc04298478ee4b6cba49774bb9db1f
hazik <Hazik@Hazik-PC>
2014-03-30 00:31:38 -0400

    Fix to GameFragment

--- src/com/awesome/lettergetter/tabs/GameFragment.java
+++ src/com/awesome/lettergetter/tabs/GameFragment.java
@@ -2,14 +2,14 @@ package com.awesome.lettergetter.tabs;
 
 import com.awesome.lettergetter.R;
 import android.os.Bundle;
-//import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+//import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 //import android.widget.ArrayAdapter;
 
-public class GameFragment extends ListFragment {
+public class GameFragment extends Fragment {
 	
 	
 //	 String[] countries = new String[] {
@@ -21,11 +21,14 @@ public class GameFragment extends ListFragment {
 //		    };
 //		 
 
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_games, container, false);
+		
+		return rootView;
 //		
 //		 ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,countries);
 //		 
@@ -34,6 +37,6 @@ public class GameFragment extends ListFragment {
 //	 
 //	        return super.onCreateView(inflater, container, savedInstanceState);
 //	    
-		return rootView;
+		
 	}
 }
